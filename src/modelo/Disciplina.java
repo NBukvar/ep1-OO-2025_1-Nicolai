e0ce89111d3b2ebbe48d008291b51e7fdead57ef
NBukvar <nbmiketen@hotmail.com>
2025-05-17 11:24:18 -0300

    commit AlunoEspecial

--- src/modelo/Disciplina.java
+++ src/modelo/Disciplina.java
@@ -1,4 +1,35 @@
 package modelo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Disciplina {
+    private String nome;
+    private String codigo;
+    private int cargaHoraria;
+    private List<String> preRequisitos;
+
+    public Disciplina(String nome, String codigo, int cargaHoraria) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.cargaHoraria = cargaHoraria;
+        this.preRequisitos = new ArrayList<>();
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void adcionaPreRequisito(String codigo) {
+        preRequisitos.add(codigo);
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public List<String> getPreRequisitos() {
+        return preRequisitos;
+    }
+    public String toString() {
+        return nome + "("+ codigo + ") - " + cargaHoraria + " horas";
+    }
 }
--- src/modelo/Turma.java
+++ src/modelo/Turma.java
@@ -1,4 +1,50 @@
 package modelo;
+import java.util.*;
 
 public class Turma {
+    private String codigoTurma;
+    private Disciplina disciplina;
+    private Professor professor;
+    private String semestre;
+    private String formaAvaliacao;
+    private boolean presencial;
+    private String sala;
+    private String horario;
+    private int capacidadeMaxima;
+    private List<Aluno> alunosMatriculados;
+
+    public Turma(String codigoTurma, Disciplina disciplina, Professor professor, String semestre, String formaAvaliacao, boolean presencial, String sala, String horario, int capacidadeMaxima) {
+        this.codigoTurma = codigoTurma;
+        this.disciplina = disciplina;
+        this.professor = professor;
+        this.semestre = semestre;
+        this.formaAvaliacao = formaAvaliacao;
+        this.presencial = presencial;
+        this.sala = presencial ? sala : "Remota";
+        this.horario = horario;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.alunosMatriculados = new ArrayList<>();
+    }
+
+    public boolean matricularAluno(Aluno aluno) {
+        if (alunosMatriculados.size() < capacidadeMaxima) {
+            alunosMatriculados.add(aluno);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Aluno> getAlunosMatriculados() {
+        return alunosMatriculados;
+    }
+    public String toString() {
+        return "Turma" +codigoTurma +
+                " - Disciplina: " + disciplina.getCodigo() +
+                " - Professor: " + professor.getNome() +
+                " - Semestre: " + semestre +
+                " - Avaliação: " + formaAvaliacao +
+                " - Tipo: " + (presencial ? "Presencial" : "Remota") +
+                " - Horário: " + horario +
+                " - Capacidade: " + capacidadeMaxima + " alunos";
+    }
 }
