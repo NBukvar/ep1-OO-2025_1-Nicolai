ea5228ece5c460aac83df619f9b930db043b2566
NBukvar <nbmiketen@hotmail.com>
2025-05-19 17:56:18 -0300

    commit Avaliacao-teste-

--- src/modelo/Avaliacao.java
+++ src/modelo/Avaliacao.java
@@ -1,6 +1,6 @@
 package modelo;
+//public interface - definir metodos para classes implementarem
+public interface Avaliacao {
+    double calcularMedia(double p1, double p2, double p3, double listas, double seminario);
 
-public class Avaliacao {
-
-    //avaliacao
 }
